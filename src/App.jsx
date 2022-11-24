b3aaf52ee912e9377722f006eb8f6efaaf3c5dcc
AMANDASALOMAO <a.salo.andrade@outlook.com>
2022-11-24 15:34:52 -0300

    UseEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import './App.css';
 import {useState} from 'react';
+import { useEffect } from "react"
 
 function App() {
-  const [name, setName] = useState("Amanda");
+ /* const [name, setName] = useState("Amanda");
   const [number, setNumber] = useState(1);
 
   console.log(name);
@@ -25,6 +26,58 @@ function App() {
   </div>
 </div>
   );
+}*/
+
+/*
+const [count, setCount] = useState(0);
+
+//1 - UseEffect utilização
+useEffect(() => {
+  console.log("Roda a cada renderização")
 }
+);
+return (
+  <div className="Effect1">
+  <div>
+    <button onClick={() => setCount ((prevCount) => prevCount + 1)}>
+      Renderizar
+    </button>
+    <p>{count}</p>
+  </div>
+</div>
+);
+}*/
+
+
+const [count, setCount] = useState(0);
+const [countB, setCountB] = useState(10);
+
+useEffect(() => {
+  console.log("Roda a cada renderização")
+}
+);
+
+// 2 - ARRAY DE DEPENDÊNCIA
+useEffect(() => {
+  console.log("Só roda ao incrementar o valor");
+}, [count]);
+
+  return (
+    <div className="Effect2">
+    <div>
+      <button onClick={() => setCount ((prevCount) => prevCount + 1)}>
+        Renderizar 
+      </button>
+      <p>{count}</p>
+    </div>
+    <div>
+      <button onClick={() => setCountB ((prevCount) => prevCount + 1)}>
+        Renderizar B
+      </button>
+      <p>{countB}</p>
+    </div>
+  </div>
+  );
+  }
 
 export default App;
